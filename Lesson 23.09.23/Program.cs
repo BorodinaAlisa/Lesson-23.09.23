613b3d5811b9c7faf2719e314b09df041fe69e9f
Алиса <@LAPTOP-QB2I6692>
2023-09-28 23:35:00 +0300

    Добавьте файлы проекта.

---
+++ Lesson 23.09.23/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_23._09._23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Упражнение 4.1. Написать программу, которая читает с экрана число от 1 до 365, переводит этот число в месяц и день месяца.");
+            Console.WriteLine("Введите число от 1 до 365.");
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number < 1 || number > 365)
+            {
+                Console.WriteLine("Ошибка.Введено некоректное число.");
+            }
+            int[] days = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+            string[] monthsname = { "января", "февраля", "марта", "апреля", "мая", "июня", "июля", "августа", "сентября", "октября", "ноября", "декабря" };
+            int month = 0;
+            int daysofmonths = 0;
+            foreach (int daysincurrentmonth in days)
+            {
+                if (number <= daysincurrentmonth)
+                {
+                    daysofmonths = number;
+                    break;
+                }
+                else
+                {
+                    number -= daysincurrentmonth;
+                    month++;
+                }
+            }
+            Console.WriteLine($"Дата: {daysofmonths} {monthsname[month]}");
+            Console.WriteLine("Упражнение 4.2. Добавить к задаче из предыдущего упражнения проверку числа введенного пользователем. Если число меньше 1 или больше 365, программа должна вырабатывать исключение, и выдавать на экран сообщение.");
+            {
+                try
+                {
+                    Console.WriteLine("Введите число от 1 до 365.");
+                    int number1 = Convert.ToInt32(Console.ReadLine());
+                    if (number1 < 1 || number1 > 365)
+                    {
+                        throw new Exception("Ошибка.Введено некоректное число.");
+                    }
+                    int[] days1 = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+                    string[] monthsName = { "января", "февраля", "марта", "апреля", "мая", "июня", "июля", "августа", "сентября", "октября", "ноября", "декабря" };
+                    int months = 0;
+                    int dayofmonths = 0;
+                    foreach (int daysincurrentmonth in days1)
+                    {
+                        if (number1 <= daysincurrentmonth)
+                        {
+                            dayofmonths = number1;
+                            break;
+                        }
+                        else
+                        {
+                            number1 -= daysincurrentmonth;
+                            month++;
+                        }
+                        Console.WriteLine($"Дата: {dayofmonths} {monthsname[months]}");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            Console.WriteLine("Домашнее задание 4.1. Изменить программу из упражнений 4.1 и 4.2 так, чтобы она учитывала год (високосный или нет).");
+            try
+            {
+                Console.WriteLine("Введите число от 1 до 365.");
+                int number2 = Convert.ToInt32(Console.ReadLine());
+                if (number2 < 1 || number2 > 365)
+                {
+                    throw new Exception("Ошибка.Введено некоректное значение.");
+                }
+                Console.WriteLine("Введите год.");
+                int year = Convert.ToInt32(Console.ReadLine());
+                bool year2 = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+                int[] daysinmonth = year2? new int[] { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 } : new int[] { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+                int month3 = 0;
+                int day = 0;
+                for (int i = 0; i < daysinmonth.Length; i++)
+                {
+                    if (number2 <= daysinmonth[i])
+                    {
+                        month3 = i + 1;
+                        day = number2;
+                        break;
+                    }
+                    else
+                    {
+                        number2 -= daysinmonth[i];
+                    }
+                }
+                Console.WriteLine($"Дата: {day}.{month3}.{year}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+            Console.ReadKey();
+        }   
+    }
+}
---
+++ Zadanie 1,2,3,4,5/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_1_2_3_4_5
+{
+    internal class Program
+    {
+        enum dayofweek
+        {
+            Понедельник = 1,
+            Вторник,
+            Среда,
+            Четверг,
+            Пятница,
+            Суббота,
+            Воскресенье,
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задание 1.Определить, является ли последовательность из 10 чисел упорядоченной по возрастанию. В случае отрицательного ответа определить порядковый номер первого числа, которое нарушает данную последовательность.");
+            Console.WriteLine("Дана последовательность из 10 чисел: 1, 4, 5, 7, 8, 11, 14, 19, 25, 2");
+            int[] sequence = new int[10] { 1, 4, 5, 7, 8, 11, 14, 19, 25, 2 };
+            int index = -1;//порядковый номер числа,нарушевшего последовательность по возростанию
+            for (int i = 0; i < sequence.Length - 1; i++)
+            {
+                if (sequence[i] > sequence[i + 1])
+                {
+                    index = i + 1;
+                    break;
+                }
+            }
+            if (index == -1)
+            {
+                Console.WriteLine("Последовательность из 10 чисел упорядочена по возрастанию.");
+            }
+            else
+            {
+                Console.WriteLine($"Последовательность из 10 чисел упорядочена не по возрастанию, она прерывается на индексе {index}");
+            }
+            Console.WriteLine("Задание 2.Написать программу, которая по заданному номеру карты k (6 <=k <= 14) определяет достоинство соответствующей карты.");
+            Console.WriteLine("Введите номер карты от 6 до 14:");
+            int k;
+            try
+            {
+                k = Convert.ToInt32(Console.ReadLine());
+                if ( k <= 6 && k >= 14)
+                {
+                    throw new Exception("Ошибка.Введено некоректное значение.");
+                }    
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+            string card;
+            switch (k)
+            {
+                case 6:
+                card = "шестерка";
+                break;
+                case 7:
+                card = "семерка";
+                break;
+                case 8:
+                card = "восьмерка";
+                break;
+                case 9:
+                card = "девятка";
+                break;
+                case 10:
+                card = "десятка";
+                break;
+                case 11:
+                card = "валет";
+                break;
+                case 12:
+                card = "дама";
+                break;
+                case 13:
+                card = "король";
+                break;
+                case 14:
+                card = "туз";
+                break;
+                default:
+                card = k.ToString();
+                break;
+            }
+            Console.WriteLine($"Достоинство карты - {card} с порядковым номером - {k}.");
+            Console.WriteLine("Задание 4.Составить программу, которая в зависимости от порядкового номера дня недели выводит на экран его название.");
+            Console.WriteLine("Введите порядковый номер дня недели от 1 до 7:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number < 1 || number > 7)
+            {
+                Console.WriteLine("Некорректный порядковый номер дня недели.");
+                return;
+            }
+            DayOfWeek dayofweek = (DayOfWeek)number;
+            Console.WriteLine($"День недели: {dayofweek}");
+            Console.WriteLine("Задание 5.Создать массив строк. При помощи foreach обойти весь массив. При встрече элемента Hello Kitty или Barbie doll необходимо положить их в “сумку”, т.е. прибавить к результату. Вывести на экран сколько кукол в “сумке”.");
+            string[] toys = { "Hello Kitty", "Barbie doll", "Toy car", "Ball", "Plush hare", "Hello Kitty" };
+            int count = 0;
+            foreach (string toy in toys)
+            {
+            if (toy == "Hello Kitty" || toy == "Barbie doll")
+            {
+            count += 1;
+            }
+            }
+            Console.WriteLine($"Количество кукол в сумке: {count}");
+            Console.WriteLine("Задание 3.Написать программу, которая принимает на входе строку и производит выходные данные в соответствии с таблицей.");
+            {
+                Console.WriteLine("Введите строку:");
+                string input = Console.ReadLine();
+                string output = GetOutput(input);
+                Console.WriteLine(output);
+            }
+            string GetOutput(string input)
+            {
+                switch (input)
+                {
+                    case "Jabroni":
+                        return "Patron Tequila";
+                    case "School Counsler":
+                        return "Anything with Alcohol";
+                    case "Programmer":
+                        return "Hipster Craft Beer";
+                    case "Bike Gang Member":
+                        return "Moonshine";
+                    case "Politician":
+                        return "Your tax dollars";
+                    case "Rapper":
+                        return "Cristal";
+                    default:
+                        return "Beer";
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
